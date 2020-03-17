4f51e2043f8b3bafabf94d59a1d768f2d1d911a4
Bagynur <baghnurkabykenov@gmail.com>
2020-03-17 22:51:16 +0600

    .

---
+++ Week7/1/Операторы цикла/For/C.py
@@ -0,0 +1,11 @@
+
+import math
+
+a = int(input())
+b = int(input())
+
+for i in range(a,b+1):
+    if(i != 0):
+        c = int(math.sqrt(i))
+        if(c*c == i):
+            print(i, sep=" ", end=" ", flush=True)
